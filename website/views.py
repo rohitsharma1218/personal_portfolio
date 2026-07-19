4c97e572ed54b5583ae691bca7d5a0964ab06f09
rohitsharma1218 <rohitsharma12012007@gmail.com>
2026-07-19 16:28:03 +0530

    Initial commit

---
+++ website/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+def home(request):
+
+    projects = [
+
+        {
+            "title": "Smart Resume Analyzer",
+            "image": "images/project1.jpg",
+            "description": "AI-powered ATS Resume Screening System built using Django.",
+            "tech": ["Python", "Django", "HTML", "CSS"],
+            "github": "#",
+            "demo": "#"
+        },
+
+        {
+            "title": "Personal Portfolio",
+            "image": "images/portfolio.jpg",
+            "description": "Modern animated portfolio created using Django and GSAP.",
+            "tech": ["html","css","JavaScript","python","Django","GSAP"],
+            "github": "#",
+            "demo": "#"
+        },
+
+        {
+            "title": "Calculator",
+            "image": "images/calculator.jpg",
+            "description": "Simple calculator built with Python and Django.",
+            "tech": ["html","css","Python", "flask"],
+            "github": "#",
+            "demo": "#"
+        }
+
+    ]
+
+    return render(request, "index.html", {
+        "projects": projects
+    })
